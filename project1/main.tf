63a19d2d9a108e9acc7d90e15b459ec333220fdc
Meet Patel <meetpateldigital5@gmail.com>
2024-09-19 15:43:02 -0400

    Update main.tf

--- project1/main.tf
+++ project1/main.tf
@@ -1,8 +1,10 @@
-provider "aws" {
+rovider "aws" {
     region = "us-east-1" # set your desired aws region
 }
 
-resources "aws_insatce" "example"{
- ami          = "ami-ncieh24rn2idce" # specifiyan appropriate AMI ID
- instace_type = "t2.micro"
- }
+resource "aws_instance" "example"{
+ ami          = "ami-0e86e20daehqdb2" # specifiyan appropriate AMI ID
+ instance_type = "t2.micro"
+ subnet_id = "subnet-09545a31sajkj"  # provide your subnet id here
+ key_name = "test1_key"  # provide your  key pair name here
+ } 
