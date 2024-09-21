b451982abdde47b0aa7ac17400d9f12b1efb0c06
Meet Patel <meetpateldigital5@gmail.com>
2024-09-21 10:55:29 -0400

    Update variable.tf

--- project2/variable.tf
+++ project2/variable.tf
@@ -1,1 +1,10 @@
+variable "ami_value"{
+   description = "value for the ami"
+}
+variable "instance_type_value"{
+   description = "value for the instace_type"
+}
+variable "subnet_id_value"{
+   description = "value for the subnet_id"
+}
 
